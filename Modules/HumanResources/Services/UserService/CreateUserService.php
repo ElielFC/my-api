666f6d9af13112760820af3d283b59c9de95719c
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-04-24 22:47:58 -0400

    Criando funsão para setar as permissões quando estiver criando o usuário.

--- Modules/HumanResources/Services/UserService/CreateUserService.php
+++ Modules/HumanResources/Services/UserService/CreateUserService.php
@@ -13,8 +13,32 @@ class CreateUserService
         $this->user_repository = $user_repository;
     }
 
+    /**
+     * Cria um novo registro de usuário
+     * @param array $attributes
+     * @return \Modules\HumanResources\Entities\User
+     */
     public function execute(array $attributes)
     {
-        return $this->user_repository->create($attributes);
+        if($attributes['control_permissions_by'] === 'I') {
+            $attributes['role_id'] = null;
+        }
+
+        $user = $this->user_repository->create($attributes);
+
+        if (!empty($attributes['permissions'])) {
+            $this->setPermissions($user, $attributes['permissions']);
+        }
+
+        return $user;
+    }
+
+    private function setPermissions($user, array $permissions)
+    {
+        if ($user->control_permissions_by === 'I') {
+            return $user->permissions()->sync($permissions);
+        }
+
+        return $user->role->permissions()->sync($permissions);
     }
 }
